abfb2d1f4c438122fe3919f1d224ae2c1a239fff
Ilya Zinkovich <ilya.zinkovich@gmail.com>
2019-01-03 23:46:54 +0100

    Finished lists exercises

--- src/test/scala/fpinscala/datastructures/ListExercises.scala
+++ src/test/scala/fpinscala/datastructures/ListExercises.scala
@@ -3,6 +3,7 @@ package fpinscala.datastructures
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
 
 class ListExercises extends FlatSpec with Matchers {
 
@@ -260,30 +261,157 @@ class ListExercises extends FlatSpec with Matchers {
   }
 
   "Exercise 3.16" should "adds 1" in {
-    def add1(list: List[Int]): List[Int] = list match {
-      case Nil => Nil
-      case Construct(head, tail) => Construct(head + 1, add1(tail))
-    }
+    import Support.foldRight
+
+    def add1(list: List[Int]): List[Int] =
+      foldRight(list, Nil: List[Int])((value, accumulator) => Construct(value + 1, accumulator))
 
     add1(List(1, 2, 3)) should be(List(2, 3, 4))
   }
 
   "Exercise 3.17" should "map to string" in {
-    def mapToString(list: List[Int]): List[String] = list match {
-      case Nil => Nil
-      case Construct(head, tail) => Construct(head.toString, mapToString(tail))
-    }
+    import Support.foldRight
+
+    def mapToString(list: List[Int]): List[String] =
+      foldRight(list, Nil: List[String])((value, accumulator) => Construct(value.toString, accumulator))
 
     mapToString(List(1, 2, 3)) should be(List("1", "2", "3"))
   }
 
   "Exercise 3.18" should "map" in {
-    def map[A, B](list: List[A])(f: A => B): List[B] = list match {
-      case Nil => Nil
-      case Construct(head, tail) => Construct(f(head), map(tail)(f))
+    import Support.foldRight
+
+    def map[A, B](list: List[A])(f: A => B): List[B] =
+      foldRight(list, Nil: List[B])((value, accumulator) => Construct(f(value), accumulator))
+
+    def mapWithMutableList[A, B](list: List[A])(f: A => B): List[B] = {
+      val mutableList = new ListBuffer[B]
+
+      def loop(list: List[A]): Unit = list match {
+        case Nil => ()
+        case Construct(head, tail) => mutableList += f(head); loop(tail)
+      }
+
+      loop(list)
+      List(mutableList.toList: _*)
     }
 
     map(List(1, 2, 3))(_ + 1) should be(List(2, 3, 4))
     map(List(1, 2, 3))(_.toString) should be(List("1", "2", "3"))
+    mapWithMutableList(List(1, 2, 3))(_ + 1) should be(List(2, 3, 4))
+    mapWithMutableList(List(1, 2, 3))(_.toString) should be(List("1", "2", "3"))
   }
+
+  "Exercise 3.19" should "filter" in {
+    import Support.foldRight
+
+    def filter[A](list: List[A])(predicate: A => Boolean): List[A] =
+      foldRight(list, Nil: List[A])((value, accumulator) =>
+        if (predicate(value)) Construct(value, accumulator) else accumulator)
+
+    def filterWithMutableList[A](list: List[A])(predicate: A => Boolean): List[A] = {
+      val mutableList = new ListBuffer[A]
+
+      def loop(list: List[A]): Unit = list match {
+        case Nil => ()
+        case Construct(head, tail) => if (predicate(head)) mutableList += head; loop(tail)
+      }
+
+      loop(list)
+      List(mutableList.toList: _*)
+    }
+
+    filter(List(1, 2, 3))(_ % 2 == 1) should be(List(1, 3))
+    filterWithMutableList(List(1, 2, 3))(_ % 2 == 1) should be(List(1, 3))
+  }
+
+  "Exercise 3.20" should "flatMap" in {
+    import Support.{concat, map}
+
+    def flatMap[A, B](list: List[A])(f: A => List[B]): List[B] = concat(map(list)(f))
+
+    flatMap(List(1, 2, 3))(i => List(i, i)) should be(List(1, 1, 2, 2, 3, 3))
+  }
+
+  "Exercise 3.20" should "filter with flatMap" in {
+    import Support.flatMap
+
+    def filter[A](list: List[A])(predicate: A => Boolean): List[A] =
+      flatMap(list)(value => if (predicate(value)) List(value) else Nil)
+
+    filter(List(1, 2, 3))(_ % 2 == 1) should be(List(1, 3))
+  }
+
+  "Exercise 3.21" should "zip with add" in {
+    def zipAdd(left: List[Int], right: List[Int]): List[Int] = (left, right) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Construct(leftHead, leftTail), Construct(rightHead, rightTail)) =>
+        Construct(leftHead + rightHead, zipAdd(leftTail, rightTail))
+    }
+
+    zipAdd(List(1, 2, 3), List(4, 5, 6)) should be(List(5, 7, 9))
+  }
+
+  "Exercise 3.22" should "zip with" in {
+    def zipWith[A, B, C](left: List[A], right: List[B])(f: (A, B) => C): List[C] = (left, right) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Construct(leftHead, leftTail), Construct(rightHead, rightTail)) =>
+        Construct(f(leftHead, rightHead), zipWith(leftTail, rightTail)(f))
+    }
+
+    zipWith(List(1, 2, 3), List(4, 5, 6))((a, b) => a + b) should be(List(5, 7, 9))
+  }
+
+  "Exercise 3.23" should "has sub-sequence" in {
+    @annotation.tailrec
+    def startsWith[A](list: List[A], prefix: List[A]): Boolean = (list, prefix) match {
+      case (_, Nil) => true
+      case (Construct(head, tail), Construct(prefixHead, prefixTail)) if head == prefixHead => startsWith(tail, prefixTail)
+      case _ => false
+    }
+
+    @annotation.tailrec
+    def hasSubsequence[A](source: List[A], subsequence: List[A]): Boolean = (source, subsequence) match {
+      case (Nil, Nil) => true
+      case (Nil, _) => false
+      case _ if startsWith(source, subsequence) => true
+      case (Construct(_, tail), _) => hasSubsequence(tail, subsequence)
+    }
+
+    hasSubsequence(List(1, 2, 3), List()) should be(true)
+    hasSubsequence(List(1, 2, 3), List(1)) should be(true)
+    hasSubsequence(List(1, 2, 3), List(1, 2)) should be(true)
+    hasSubsequence(List(1, 2, 3), List(2, 3)) should be(true)
+    hasSubsequence(List(1, 2, 3), List(1, 3)) should be(false)
+  }
+
+  object Support {
+
+    @tailrec
+    def foldLeft[A, B](source: List[A], accumulator: B)(f: (B, A) => B): B = source match {
+      case Nil => accumulator
+      case Construct(head, tail) => foldLeft(tail, f(accumulator, head))(f)
+    }
+
+    def foldRight[A, B](source: List[A], seed: B)(f: (A, B) => B): B =
+      foldLeft(reverse(source), seed)((a, b) => f(b, a))
+
+    def reverse[A](source: List[A]): List[A] =
+      foldLeft(source, Nil: List[A])((accumulator, value) => Construct(value, accumulator))
+
+    def append[A](source: List[A], elements: List[A]): List[A] =
+      foldLeft(source, elements)((accumulator, value) => Construct(value, accumulator))
+
+    def concat[A](source: List[List[A]]): List[A] = {
+      reverse(foldLeft(source, Nil: List[A])((accumulator, value) => append(value, accumulator)))
+    }
+
+    def map[A, B](list: List[A])(f: A => B): List[B] =
+      foldRight(list, Nil: List[B])((value, accumulator) => Construct(f(value), accumulator))
+
+    def flatMap[A, B](list: List[A])(f: A => List[B]): List[B] = concat(map(list)(f))
+  }
+
 }

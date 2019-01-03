e53e76d8a6465e5957faac55f3f28439325174df
Ilya Zinkovich <ilya.zinkovich@gmail.com>
2019-01-03 22:54:59 +0100

    Added map and formatted

--- src/test/scala/fpinscala/datastructures/ListExercises.scala
+++ src/test/scala/fpinscala/datastructures/ListExercises.scala
@@ -11,6 +11,7 @@ class ListExercises extends FlatSpec with Matchers {
       case Nil => 0
       case Construct(head, tail) => head + sum(tail)
     }
+
     val result = List(1, 2, 3, 4, 5) match {
       case Construct(x, Construct(2, Construct(4, _))) => x
       case Nil => 42
@@ -18,7 +19,7 @@ class ListExercises extends FlatSpec with Matchers {
       case Construct(h, t) => h + sum(t)
       case _ => 101
     }
-    result should be (3)
+    result should be(3)
   }
 
   "Exercise 3.2" should "tail" in {
@@ -27,10 +28,11 @@ class ListExercises extends FlatSpec with Matchers {
       case Construct(_, Nil) => Nil
       case Construct(_, tail) => tail
     }
-    tail(List()) should be (List())
-    tail(List(1)) should be (List())
-    tail(List(1, 2)) should be (List(2))
-    tail(List(1, 2, 3)) should be (List(2, 3))
+
+    tail(List()) should be(List())
+    tail(List(1)) should be(List())
+    tail(List(1, 2)) should be(List(2))
+    tail(List(1, 2, 3)) should be(List(2, 3))
   }
 
   "Exercise 3.3" should "set head" in {
@@ -38,9 +40,10 @@ class ListExercises extends FlatSpec with Matchers {
       case Nil => Nil
       case Construct(_, tail) => Construct(newHead, tail)
     }
-    setHead(1, List()) should be (List())
-    setHead(2, List(1)) should be (List(2))
-    setHead(3, List(1, 2)) should be (List(3, 2))
+
+    setHead(1, List()) should be(List())
+    setHead(2, List(1)) should be(List(2))
+    setHead(3, List(1, 2)) should be(List(3, 2))
   }
 
   "Exercise 3.4" should "drop" in {
@@ -53,15 +56,16 @@ class ListExercises extends FlatSpec with Matchers {
           else drop(tail, n - 1)
       }
     }
-    drop(List(), 1) should be (List())
-    drop(List(1), 1) should be (List())
-    drop(List(1, 2), 1) should be (List(2))
-    drop(List(1, 2), 2) should be (List())
-    drop(List(1, 2, 3), 1) should be (List(2, 3))
-    drop(List(1, 2, 3), 0) should be (List(1, 2, 3))
-    drop(List(1, 2, 3), -1) should be (List(1, 2, 3))
-    drop(List(), 0) should be (List())
-    drop(List(), -1) should be (List())
+
+    drop(List(), 1) should be(List())
+    drop(List(1), 1) should be(List())
+    drop(List(1, 2), 1) should be(List(2))
+    drop(List(1, 2), 2) should be(List())
+    drop(List(1, 2, 3), 1) should be(List(2, 3))
+    drop(List(1, 2, 3), 0) should be(List(1, 2, 3))
+    drop(List(1, 2, 3), -1) should be(List(1, 2, 3))
+    drop(List(), 0) should be(List())
+    drop(List(), -1) should be(List())
   }
 
   "Exercise 3.5" should "dropWhile" in {
@@ -70,17 +74,19 @@ class ListExercises extends FlatSpec with Matchers {
       case original@Construct(head, tail) =>
         if (predicate(head)) dropWhile(tail, predicate) else original
     }
+
     def dropWhileWithTypeInference[A](list: List[A])(predicate: A => Boolean): List[A] = list match {
       case Nil => Nil
       case original@Construct(head, tail) =>
         if (predicate(head)) dropWhile(tail, predicate) else original
     }
+
     val predicate: Int => Boolean = (number: Int) => number < 2
-    dropWhile(List(), predicate) should be (List())
-    dropWhile(List(1), predicate) should be (List())
-    dropWhile(List(1, 2), predicate) should be (List(2))
-    dropWhile(List(1, 2, 3), predicate) should be (List(2, 3))
-    dropWhileWithTypeInference(List(1, 2, 3))(_ < 2) should be (List(2, 3))
+    dropWhile(List(), predicate) should be(List())
+    dropWhile(List(1), predicate) should be(List())
+    dropWhile(List(1, 2), predicate) should be(List(2))
+    dropWhile(List(1, 2, 3), predicate) should be(List(2, 3))
+    dropWhileWithTypeInference(List(1, 2, 3))(_ < 2) should be(List(2, 3))
   }
 
   "Exercise 3.+" should "append" in {
@@ -88,7 +94,8 @@ class ListExercises extends FlatSpec with Matchers {
       case Nil => elements
       case Construct(head, tail) => Construct(head, append(tail, elements))
     }
-    append(List(1), List(2, 3, 4)) should be (List(1, 2, 3, 4))
+
+    append(List(1), List(2, 3, 4)) should be(List(1, 2, 3, 4))
   }
 
   "Exercise 3.6" should "init" in {
@@ -97,9 +104,11 @@ class ListExercises extends FlatSpec with Matchers {
       case Construct(_, Nil) => Nil
       case Construct(head, tail) => Construct(head, init(tail))
     }
+
     def initEfficient[A](source: List[A]): List[A] = {
       import collection.mutable.ListBuffer
       val buf = new ListBuffer[A]
+
       @annotation.tailrec
       def loop(current: List[A]): List[A] = current match {
         case Nil => Nil
@@ -108,10 +117,12 @@ class ListExercises extends FlatSpec with Matchers {
           buf += head
           loop(tail)
       }
+
       loop(source)
     }
-    init(List(1, 2, 3)) should be (List(1, 2))
-    initEfficient(List(1, 2, 3)) should be (List(1, 2))
+
+    init(List(1, 2, 3)) should be(List(1, 2))
+    initEfficient(List(1, 2, 3)) should be(List(1, 2))
   }
 
   "Exercise 3.++" should "append" in {
@@ -119,17 +130,20 @@ class ListExercises extends FlatSpec with Matchers {
       case Nil => initialValue
       case Construct(head, tail) => combine(head, foldRight(tail, initialValue)(combine))
     }
+
     def sum(ints: List[Int]): Int = foldRight(ints, 0)(_ + _)
+
     def product(doubles: List[Double]): Double = foldRight(doubles, 1.0)(_ * _)
-    sum(List(1, 2, 3, 4)) should be (10)
-    product(List(1, 2, 3, 4)) should be (24.0)
+
+    sum(List(1, 2, 3, 4)) should be(10)
+    product(List(1, 2, 3, 4)) should be(24.0)
   }
 
   "Exercise 3.7" should "short-circuit foldRight" in {
     """No, this is not possible!
-        |The reason is because _before_ we ever call our function, `f`, we evaluate its argument,
-        |which in the case of `foldRight` means traversing the list all the way to the end.
-        |We need _non-strict_ evaluation to support early termination""" shouldNot be ("")
+      |The reason is because _before_ we ever call our function, `f`, we evaluate its argument,
+      |which in the case of `foldRight` means traversing the list all the way to the end.
+      |We need _non-strict_ evaluation to support early termination""" shouldNot be("")
   }
 
   "Exercise 3.8" should "foldRight with Nil and Cons" in {
@@ -137,7 +151,8 @@ class ListExercises extends FlatSpec with Matchers {
       case Nil => initialValue
       case Construct(head, tail) => combine(head, foldRight(tail, initialValue)(combine))
     }
-    foldRight(List(1, 2, 3), Nil: List[Int])(Construct(_, _)) should be (List(1, 2, 3))
+
+    foldRight(List(1, 2, 3), Nil: List[Int])(Construct(_, _)) should be(List(1, 2, 3))
   }
 
   "Exercise 3.9" should "length" in {
@@ -145,9 +160,11 @@ class ListExercises extends FlatSpec with Matchers {
       case Nil => seed
       case Construct(head, tail) => f(head, foldRight(tail, seed)(f))
     }
+
     def length[A](source: List[A]): Int = foldRight(source, 0)((_, accumulator) => accumulator + 1)
-    length(List()) should be (0)
-    length(List(1, 2, 3)) should be (3)
+
+    length(List()) should be(0)
+    length(List(1, 2, 3)) should be(3)
   }
 
   "Exercise 3.10" should "foldLeft" in {
@@ -156,8 +173,9 @@ class ListExercises extends FlatSpec with Matchers {
       case Nil => accumulator
       case Construct(head, tail) => foldLeft(tail, f(accumulator, head))(f)
     }
-    foldLeft(List(1, 2, 3, 4), 0)(_ + _) should be (10)
-    foldLeft(List(1, 2, 3, 4), 1.0)(_ * _) should be (24.0)
+
+    foldLeft(List(1, 2, 3, 4), 0)(_ + _) should be(10)
+    foldLeft(List(1, 2, 3, 4), 1.0)(_ * _) should be(24.0)
   }
 
   "Exercise 3.11" should "sum, product and length with foldLeft" in {
@@ -166,12 +184,16 @@ class ListExercises extends FlatSpec with Matchers {
       case Nil => accumulator
       case Construct(head, tail) => foldLeft(tail, f(accumulator, head))(f)
     }
+
     def length[A](source: List[A]): Int = foldLeft(source, 0)((accumulator, _) => accumulator + 1)
+
     def sum(ints: List[Int]): Int = foldLeft(ints, 0)(_ + _)
+
     def product(doubles: List[Double]): Double = foldLeft(doubles, 1.0)(_ * _)
-    sum(List(1, 2, 3, 4)) should be (10)
-    product(List(1, 2, 3, 4)) should be (24)
-    length(List(1, 2, 3, 4)) should be (4)
+
+    sum(List(1, 2, 3, 4)) should be(10)
+    product(List(1, 2, 3, 4)) should be(24)
+    length(List(1, 2, 3, 4)) should be(4)
   }
 
   "Exercise 3.12" should "reverse" in {
@@ -180,9 +202,11 @@ class ListExercises extends FlatSpec with Matchers {
       case Nil => accumulator
       case Construct(head, tail) => foldLeft(tail, f(accumulator, head))(f)
     }
+
     def reverse[A](source: List[A]): List[A] =
       foldLeft(source, Nil: List[A])((accumulator, value) => Construct(value, accumulator))
-    reverse(List(1, 2, 3)) should be (List(3, 2, 1))
+
+    reverse(List(1, 2, 3)) should be(List(3, 2, 1))
   }
 
   "Exercise 3.13" should "foldRight through foldLeft" in {
@@ -191,12 +215,15 @@ class ListExercises extends FlatSpec with Matchers {
       case Nil => accumulator
       case Construct(head, tail) => foldLeft(tail, f(accumulator, head))(f)
     }
+
     def reverse[A](source: List[A]): List[A] =
       foldLeft(source, Nil: List[A])((accumulator, value) => Construct(value, accumulator))
+
     def foldRight[A, B](source: List[A], seed: B)(f: (A, B) => B): B =
       foldLeft(reverse(source), seed)((a, b) => f(b, a))
-    foldRight(List(1, 2, 3, 4), 0)(_ + _) should be (10)
-    foldRight(List(1, 2, 3, 4), 1.0)(_ * _) should be (24.0)
+
+    foldRight(List(1, 2, 3, 4), 0)(_ + _) should be(10)
+    foldRight(List(1, 2, 3, 4), 1.0)(_ * _) should be(24.0)
   }
 
   "Exercise 3.14" should "append" in {
@@ -205,9 +232,11 @@ class ListExercises extends FlatSpec with Matchers {
       case Nil => accumulator
       case Construct(head, tail) => foldLeft(tail, f(accumulator, head))(f)
     }
+
     def append[A](source: List[A], elements: List[A]): List[A] =
       foldLeft(source, elements)((accumulator, value) => Construct(value, accumulator))
-    append(List(1), List(2, 3, 4)) should be (List(1, 2, 3, 4))
+
+    append(List(1), List(2, 3, 4)) should be(List(1, 2, 3, 4))
   }
 
   "Exercise 3.15" should "concat" in {
@@ -216,13 +245,45 @@ class ListExercises extends FlatSpec with Matchers {
       case Nil => accumulator
       case Construct(head, tail) => foldLeft(tail, f(accumulator, head))(f)
     }
+
     def reverse[A](source: List[A]): List[A] =
       foldLeft(source, Nil: List[A])((accumulator, value) => Construct(value, accumulator))
+
     def append[A](source: List[A], elements: List[A]): List[A] =
       foldLeft(source, elements)((accumulator, value) => Construct(value, accumulator))
+
     def concat[A](source: List[List[A]]): List[A] = {
       reverse(foldLeft(source, Nil: List[A])((accumulator, value) => append(value, accumulator)))
     }
-    concat(List(List(1), List(2, 3, 4), List(5, 6))) should be (List(1, 2, 3, 4, 5, 6))
+
+    concat(List(List(1), List(2, 3, 4), List(5, 6))) should be(List(1, 2, 3, 4, 5, 6))
+  }
+
+  "Exercise 3.16" should "adds 1" in {
+    def add1(list: List[Int]): List[Int] = list match {
+      case Nil => Nil
+      case Construct(head, tail) => Construct(head + 1, add1(tail))
+    }
+
+    add1(List(1, 2, 3)) should be(List(2, 3, 4))
+  }
+
+  "Exercise 3.17" should "map to string" in {
+    def mapToString(list: List[Int]): List[String] = list match {
+      case Nil => Nil
+      case Construct(head, tail) => Construct(head.toString, mapToString(tail))
+    }
+
+    mapToString(List(1, 2, 3)) should be(List("1", "2", "3"))
+  }
+
+  "Exercise 3.18" should "map" in {
+    def map[A, B](list: List[A])(f: A => B): List[B] = list match {
+      case Nil => Nil
+      case Construct(head, tail) => Construct(f(head), map(tail)(f))
+    }
+
+    map(List(1, 2, 3))(_ + 1) should be(List(2, 3, 4))
+    map(List(1, 2, 3))(_.toString) should be(List("1", "2", "3"))
   }
 }
--- src/test/scala/fpinscala/gettingstarted/GettingStartedExercises.scala
+++ src/test/scala/fpinscala/gettingstarted/GettingStartedExercises.scala
@@ -14,8 +14,10 @@ class GettingStartedExercises extends FlatSpec with Matchers {
         else if (number == 1) previous + current
         else loop(number - 1, current, previous + current)
       }
+
       loop(number, 0, 1)
     }
+
     fibonacci(0) should be(0)
     fibonacci(1) should be(1)
     fibonacci(5) should be(8)
@@ -28,34 +30,39 @@ class GettingStartedExercises extends FlatSpec with Matchers {
         if (n == array.length - 1) acc
         else loop(n + 1, ordered(array(n), array(n + 1)))
       }
+
       loop(0, acc = true)
     }
+
     val ordered: (Int, Int) => Boolean = (left: Int, right: Int) => left <= right
-    isSorted(Array(1, 2, 3), ordered) should be (true)
-    isSorted(Array(1, 3, 2), ordered) should be (false)
+    isSorted(Array(1, 2, 3), ordered) should be(true)
+    isSorted(Array(1, 3, 2), ordered) should be(false)
   }
 
   "Exercise 2.3" should "curry" in {
     def curry[A, B, C](f: (A, B) => C): A => B => C = {
       a: A => (b: B) => f(a, b)
     }
+
     val curriedSum = curry((a: Int, b: Int) => a + b)
-    curriedSum(1)(2) should be (3)
+    curriedSum(1)(2) should be(3)
   }
 
   "Exercise 2.4" should "uncurry" in {
     def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
       (a, b) => f(a)(b)
     }
+
     val uncurriedSum = uncurry(((a: Int, b: Int) => a + b).curried)
-    uncurriedSum(1, 2) should be (3)
+    uncurriedSum(1, 2) should be(3)
   }
 
   "Exercise 2.5" should "compose" in {
     def compose[A, B, C](f: B => C, g: A => B): A => C = {
       a: A => f(g(a))
     }
+
     val composition = compose((b: Int) => b + 1, (a: Int) => a + 2)
-    composition(1) should be (4)
+    composition(1) should be(4)
   }
 }

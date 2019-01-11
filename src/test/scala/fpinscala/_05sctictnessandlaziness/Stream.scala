4ab9620dacf0af0c2ff319372b6d1cc4cf1402a8
Ilya Zinkovich <ilya.zinkovich@gmail.com>
2019-01-12 00:38:38 +0100

    Completed 5.1-5.7

--- src/test/scala/fpinscala/_05sctictnessandlaziness/Stream.scala
+++ src/test/scala/fpinscala/_05sctictnessandlaziness/Stream.scala
@@ -1,6 +1,75 @@
 package fpinscala._05sctictnessandlaziness
 
-sealed trait Stream[+A]
+import scala.collection.mutable.ListBuffer
+
+sealed trait Stream[+A] {
+
+  import Stream._
+
+  def toList: List[A] = {
+    val list = ListBuffer[A]()
+
+    def loop(stream: Stream[A]): List[A] = stream match {
+      case Empty => list.toList
+      case Cons(head, tail) => list.append(head()); loop(tail())
+    }
+
+    loop(this)
+  }
+
+  def toListStackUnsafe: List[A] = this match {
+    case Empty => Nil
+    case Cons(head, tail) => head() :: tail().toListStackUnsafe
+  }
+
+  def take(n: Int): Stream[A] = this match {
+    case Cons(head, tail) if n > 1 => cons(head(), tail().take(n - 1))
+    case Cons(head, _) if n == 1 => cons(head(), empty)
+    case _ => this
+  }
+
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(_, tail) if n > 0 => tail().drop(n - 1)
+    case _ => this
+  }
+
+  def takeWhile(predicate: A => Boolean): Stream[A] = this match {
+    case Cons(head, tail) if predicate(head()) => cons(head(), tail().takeWhile(predicate))
+    case _ => Empty
+  }
+
+  def foldRight[B](seed: B)(f: (A, => B) => B): B = this match {
+    case Empty => seed
+    case Cons(head, tail) => f(head(), tail().foldRight(seed)(f))
+  }
+
+  def exists(predicate: A => Boolean): Boolean = {
+    foldRight(false)((a, b) => predicate(a) || b)
+  }
+
+  def forAll(predicate: A => Boolean): Boolean = {
+    foldRight(true)((a, b) => predicate(a) && b)
+  }
+
+  def takeWhileViaFoldRight(predicate: A => Boolean): Stream[A] =
+    foldRight(empty[A])((a, b) => if (predicate(a)) cons(a, b) else empty)
+
+  def headOption(): Option[A] =
+    foldRight(None: Option[A])((a, _) => Some(a))
+
+  def map[B](f: A => B): Stream[B] =
+    foldRight(empty[B])((a, b) => cons(f(a), b))
+
+  def filter(predicate: A => Boolean): Stream[A] =
+    foldRight(empty[A])((a, b) => if (predicate(a)) cons(a, b) else b)
+
+  def append[B >: A](stream: Stream[B]): Stream[B] =
+    foldRight(stream)((a, b) => cons(a, b))
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] = {
+    foldRight(empty[B])((a, b) => f(a).append(b))
+  }
+}
 
 case object Empty extends Stream[Nothing]
 
--- src/test/scala/fpinscala/_05sctictnessandlaziness/StreamExercises.scala
+++ src/test/scala/fpinscala/_05sctictnessandlaziness/StreamExercises.scala
@@ -5,6 +5,58 @@ import org.scalatest.{FlatSpec, Matchers}
 class StreamExercises extends FlatSpec with Matchers {
 
   "Exercise 5.1" should "toList" in {
+    def square(number: Int): Int = {
+      val result = number * number
+      println(result)
+      result
+    }
 
+    val stream = Stream(square(1), square(2), square(3))
+    stream.toList should be(List(1, 4, 9))
+    stream.toListStackUnsafe should be(List(1, 4, 9))
+  }
+
+  "Exercise 5.2" should "take and drop" in {
+    val stream = Stream(1, 2, 3)
+
+    stream.take(2).toList should be(List(1, 2))
+    stream.drop(1).toList should be(List(2, 3))
+  }
+
+  "Exercise 5.3" should "takeWhile" in {
+    val stream = Stream(1, 2, 3)
+
+    stream.takeWhile(_ < 3).toList should be(List(1, 2))
+    stream.takeWhile(_ % 2 == 1).toList should be(List(1))
+  }
+
+  "Exercise 5.4" should "forAll" in {
+    val stream = Stream(1, 2, 3)
+
+    stream.forAll(_ < 4) should be(true)
+    stream.forAll(_ % 2 == 1) should be(false)
+  }
+
+  "Exercise 5.5" should "takeWhile via foldRight" in {
+    val stream = Stream(1, 2, 3)
+
+    stream.takeWhileViaFoldRight(_ < 3).toList should be(List(1, 2))
+    stream.takeWhileViaFoldRight(_ % 2 == 1).toList should be(List(1))
+  }
+
+  "Exercise 5.6" should "headOption via foldRight" in {
+    Stream().headOption() should be(None)
+    Stream(1, 2, 3).headOption() should be(Some(1))
+  }
+
+  "Exercise 5.7" should "map, filter, append, and flatMap" in {
+    val stream = Stream(1, 2, 3)
+
+    stream.map(_ * 2).toList should be(List(2, 4, 6))
+    stream.filter(_ < 0).toList should be(List())
+    stream.filter(_ % 2 == 1).toList should be(List(1, 3))
+    stream.append(Stream(4, 5, 6)).toList should be(List(1, 2, 3, 4, 5, 6))
+    stream.append(Stream()).toList should be(stream.toList)
+    stream.flatMap(value => Stream(value * 2)).toList should be(List(2, 4, 6))
   }
 }

fe21b9e8764aa16a7ba026d5a684eabde8ca9eeb
Ilya Zinkovich <ilya.zinkovich@gmail.com>
2019-01-15 13:53:06 +0100

    Added final solutions to Property Testing exercises

--- src/test/scala/fpinscala/_08property/Prop.scala
+++ src/test/scala/fpinscala/_08property/Prop.scala
@@ -1,7 +1,9 @@
 package fpinscala._08property
 
-import fpinscala._06state.{RNG, State}
-import fpinscala._08property.Prop.{FailedCase, SuccessCount, TestCases}
+import java.lang.System.currentTimeMillis
+
+import fpinscala._06state.{LinearCongruentalRNG, RNG, State}
+import fpinscala._08property.Prop.{FailedCase, MaxSize, SuccessCount, TestCases}
 
 trait PropWithBooleanCheck {
 
@@ -12,11 +14,19 @@ trait PropWithBooleanCheck {
   }
 }
 
-case class Gen[A](sample: State[RNG, A]) {
+case class Gen[+A](sample: State[RNG, A]) {
 
   def flatMap[B](f: A => Gen[B]): Gen[B] = Gen(sample.flatMap(a => f(a).sample))
 
   def listOfN(size: Gen[Int]): Gen[List[A]] = size.flatMap(n => Gen.listOfN(n, this))
+
+  def unsized: SGen[A] = SGen(_ => this)
+
+  def map[B](f: A => B): Gen[B] = Gen(sample.map(f))
+
+  def map2[B, C](g: Gen[B])(f: (A, B) => C): Gen[C] = Gen(sample.map2(g.sample)(f))
+
+  def **[B](g: Gen[B]): Gen[(A,B)] = (this map2 g)((_,_))
 }
 
 object Gen {
@@ -38,32 +48,45 @@ object Gen {
       val g1Threshold = g1._2.abs / (g1._2.abs + g2._2.abs)
       if (randomDouble < g1Threshold) g1._1 else g2._1
     })
+
+  def listOf[A](g: Gen[A]): SGen[List[A]] = SGen(size => listOfN(size, g))
+
+  def listOf1[A](g: Gen[A]): SGen[List[A]] = SGen(size => listOfN(size max 1, g))
+}
+
+case class SGen[+A](forSize: Int => Gen[A]) {
+
+  def flatMap[B](f: A => SGen[B]): SGen[B] =
+    SGen(size => forSize(size).flatMap(f(_).forSize(size)))
 }
 
 sealed trait Result
+
+case object Proved extends Result
 
 case object Passed extends Result
 
 case class Falsified(failure: FailedCase, successCount: SuccessCount) extends Result
 
-case class Prop(run: (TestCases, RNG) => Result) {
+case class Prop(run: (MaxSize, TestCases, RNG) => Result) {
 
-  def &&(prop: Prop): Prop = Prop((testCases: TestCases, rng: RNG) => {
-    run(testCases, rng) match {
-      case Passed => prop.run(testCases, rng)
+  def &&(prop: Prop): Prop = Prop((maxSize, testCases, rng) => {
+    run(maxSize, testCases, rng) match {
+      case Passed => prop.run(maxSize, testCases, rng)
+      case Proved => prop.run(maxSize, testCases, rng)
       case falsified => falsified
     }
   })
 
-  def ||(prop: Prop): Prop = Prop((testCases: TestCases, rng: RNG) => {
-    run(testCases, rng) match {
-      case Falsified(failure, _) => prop.tag(failure).run(testCases, rng)
-      case Passed => prop.run(testCases, rng)
+  def ||(prop: Prop): Prop = Prop((maxSize, testCases, rng) => {
+    run(maxSize, testCases, rng) match {
+      case Falsified(failure, _) => prop.tag(failure).run(maxSize, testCases, rng)
+      case passedOrProved => prop.run(maxSize, testCases, rng)
     }
   })
 
-  def tag(msg: String) = Prop((testCases: TestCases, rng: RNG) => {
-    run(testCases, rng) match {
+  def tag(msg: String) = Prop((maxSize, testCases, rng) => {
+    run(maxSize, testCases, rng) match {
       case Falsified(failure, successCount) => Falsified(msg + "\n" + failure, successCount)
       case passed => passed
     }
@@ -75,10 +98,22 @@ object Prop {
   type SuccessCount = Int
   type FailedCase = String
   type TestCases = Int
+  type MaxSize = Int
 
   import fpinscala._05laziness.Stream
 
-  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop((testCases, rng) => {
+  def check(p: => Boolean): Prop = Prop((_, _, _) => if (p) Proved else Falsified("()", 0))
+
+  def forAll[A](g: SGen[A])(f: A => Boolean): Prop = forAll(g.forSize(_))(f)
+
+  def forAll[A](g: Int => Gen[A])(f: A => Boolean): Prop = Prop { (maxSize, testCases, rng) =>
+    val casesPerSize = (testCases + (maxSize - 1)) / maxSize
+    val props: Stream[Prop] = Stream.from(0).take((testCases min maxSize) + 1).map(i => forAll(g(i))(f))
+    val prop: Prop = props.map(p => Prop((max, _, rng) => p.run(max, casesPerSize, rng))).toList.reduce(_ && _)
+    prop.run(maxSize, testCases, rng)
+  }
+
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop((maxSize, testCases, rng) => {
     randomStream(as)(rng).zip(Stream.from(0)).take(testCases).map {
       case (generatedValue, testCase) => try {
         if (f(generatedValue)) Passed else Falsified(generatedValue.toString, testCase)
@@ -98,4 +133,17 @@ object Prop {
       s"generated an exception: ${e.getMessage}\n" +
       s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
   }
+
+  def run(p: Prop,
+          maxSize: Int = 100,
+          testCases: Int = 100,
+          rng: RNG = LinearCongruentalRNG(currentTimeMillis)): Unit =
+    p.run(maxSize, testCases, rng) match {
+      case Falsified(msg, n) =>
+        println(s"! Falsified after $n passed tests:\n $msg")
+      case Passed =>
+        println(s"+ OK, passed $testCases tests.")
+      case Proved =>
+        println(s"+ OK, proved property.")
+    }
 }
--- src/test/scala/fpinscala/_08property/PropertyExercises.scala
+++ src/test/scala/fpinscala/_08property/PropertyExercises.scala
@@ -1,6 +1,12 @@
 package fpinscala._08property
 
+import java.util.concurrent.Executors.{newCachedThreadPool, newFixedThreadPool}
+
 import fpinscala._06state.LinearCongruentalRNG
+import fpinscala._07parallelism.Par
+import fpinscala._07parallelism.Par.Par
+import fpinscala._08property.Gen._
+import fpinscala._08property.Prop.forAll
 import org.scalatest.{FlatSpec, Matchers}
 
 class PropertyExercises extends FlatSpec with Matchers {
@@ -36,40 +42,151 @@ class PropertyExercises extends FlatSpec with Matchers {
 
   "Exercise 8.4" should "Gen.choose" in {
     val rng = LinearCongruentalRNG(1)
-    Gen.choose(5, 10).sample.run(rng)._1 should be(8)
+    choose(5, 10).sample.run(rng)._1 should be(8)
   }
 
   "Exercise 8.5" should "unit boolean and listOfN" in {
     val rng = LinearCongruentalRNG(1)
-    Gen.unit(2).sample.run(rng)._1 should be(2)
+    unit(2).sample.run(rng)._1 should be(2)
     Gen.boolean.sample.run(rng)._1 should be(false)
-    Gen.listOfN(3, Gen.unit(4)).sample.run(rng)._1 should be(List(4, 4, 4))
+    Gen.listOfN(3, unit(4)).sample.run(rng)._1 should be(List(4, 4, 4))
   }
 
   "Exercise 8.6" should "flatMap and listOfN" in {
     val rng = LinearCongruentalRNG(1)
-    Gen.unit(10).flatMap(Gen.choose(0, _)).sample.run(rng)._1 should be(8)
-    Gen.unit(4).listOfN(Gen.unit(3)).sample.run(rng)._1 should be(List(4, 4, 4))
+    unit(10).flatMap(choose(0, _)).sample.run(rng)._1 should be(8)
+    unit(4).listOfN(unit(3)).sample.run(rng)._1 should be(List(4, 4, 4))
   }
 
   "Exercise 8.7" should "union" in {
     val rng = LinearCongruentalRNG(1)
-    Gen.union(Gen.unit(0), Gen.unit(1)).sample.run(rng)._1 should be(1)
+    Gen.union(unit(0), unit(1)).sample.run(rng)._1 should be(1)
   }
 
   "Exercise 8.8" should "weighted" in {
     val rng = LinearCongruentalRNG(1)
-    Gen.weighted((Gen.unit(0), 0.9), (Gen.unit(1), 0.1)).sample.run(rng)._1 should be(0)
+    weighted((unit(0), 0.9), (unit(1), 0.1)).sample.run(rng)._1 should be(0)
   }
 
   "Exercise 8.9" should "&& and ||" in {
     val rng = LinearCongruentalRNG(1)
-    val passedProperty = Prop.forAll(Gen.unit(1))(_ == 1)
-    val falsifiedProperty = Prop.forAll(Gen.unit(1))(_ == 2)
+    val passedProperty = Prop.forAll(unit(1))(_ == 1)
+    val falsifiedProperty = Prop.forAll(unit(1))(_ == 2)
     val testCases = 10
-    (passedProperty && passedProperty).run(testCases, rng) should be(Passed)
-    (passedProperty && falsifiedProperty).run(testCases, rng) shouldNot be(Passed)
-    (falsifiedProperty || passedProperty).run(testCases, rng) should be(Passed)
-    (falsifiedProperty || falsifiedProperty).run(testCases, rng) shouldNot be(Passed)
+    val maxSize = 10
+    (passedProperty && passedProperty).run(maxSize, testCases, rng) should be(Passed)
+    (passedProperty && falsifiedProperty).run(maxSize, testCases, rng) shouldNot be(Passed)
+    (falsifiedProperty || passedProperty).run(maxSize, testCases, rng) should be(Passed)
+    (falsifiedProperty || falsifiedProperty).run(maxSize, testCases, rng) shouldNot be(Passed)
   }
+
+  "Exercise 8.10" should "Gen unsized" in {
+    val rng = LinearCongruentalRNG(1)
+    val unsizedGen: SGen[Int] = unit(1).unsized
+    unsizedGen.forSize(10).sample.run(rng)._1 should be(1)
+  }
+
+  "Exercise 8.11" should "SGen flatMap" in {
+    val rng = LinearCongruentalRNG(1)
+    val sgen1 = unit(1).unsized
+    val sgen2 = unit(2).unsized
+    sgen1.flatMap(_ => sgen2).forSize(1).sample.run(rng)._1 should be(2)
+  }
+
+  "Exercise 8.12" should "listOf" in {
+    val rng = LinearCongruentalRNG(1)
+    val sgen = listOf(unit(1))
+    sgen.forSize(3).sample.run(rng)._1 should be(List(1, 1, 1))
+  }
+
+  "Exercise 8.13" should "test max with listOf1" in {
+    val smallInt = choose(-10, 10)
+    val maxProp = forAll(listOf1(smallInt)) { ns =>
+      val max = ns.max
+      !ns.exists(_ > max)
+    }
+    Prop.run(maxProp)
+  }
+
+  "Exercise 8.14" should "test sorted" in {
+    val smallInt = choose(-10, 10)
+    val sortProp = forAll(listOf(smallInt)) { list =>
+      val sortedList = list.sorted
+      sortedList.isEmpty || sortedList.tail.isEmpty || sortedList.zip(sortedList.tail).forall {
+        case (left, right) => left <= right
+      } && sortedList.size == list.size &&
+        sortedList.forall(element => list.contains(element)) && list.forall(element => sortedList.contains(element))
+    }
+    Prop.run(sortProp)
+  }
+
+  "Exercise 8.15" should "test small domains exhaustively" in {
+    /*
+    Book Answer:
+    You will need to add to the representation of `Gen`.
+    For example, `Gen[Int]` should be capable of generating random integers
+    as well as generating a stream of all the integers from `Int.MinValue` to `Int.MaxValue`.
+    You may want to have the behavior depend on how many test cases were requested.
+     */
+  }
+
+  "Exercise 8.16" should "test Par" in {
+    /* A `Gen[Par[Int]]` generated from a list summation that spawns a new parallel
+     * computation for each element of the input list summed to produce the final
+     * result. This is not the most compelling example, but it provides at least some
+     * variation in structure to use for testing.
+     */
+    val parGen: Gen[Par[Int]] = choose(-100, 100)
+      .listOfN(choose(0, 20))
+      .map(list => list.foldLeft(Par.unit(0))((par, integer) => Par.fork {
+        Par.map2(par, Par.unit(integer))(_ + _)
+      }))
+  }
+
+  "Exercise 8.17" should "test fork" in {
+    def equal[A](p: Par[A], p2: Par[A]): Par[Boolean] = Par.map2(p, p2)(_ == _)
+
+    val S = weighted(
+      choose(1000, 2000).map(newFixedThreadPool) -> .100,
+      unit(newCachedThreadPool) -> .0
+    )
+
+    def forAllPar[A](g: Gen[A])(f: A => Par[Boolean]): Prop = forAll(S ** g) {
+      case s ** a => f(a)(s).get
+    }
+
+    val parGen: Gen[Par[Int]] = choose(-10, 10)
+      .listOfN(choose(0, 2))
+      .map(list => list.foldLeft(Par.unit(0))((par, integer) => Par.fork {
+        Par.map2(par, Par.unit(integer))(_ + _)
+      }))
+
+    val forkProp = forAllPar(parGen)(i => equal(Par.fork(i), i)) tag "fork"
+    //    Prop.run(forkProp)
+  }
+
+  "Exercise 8.18" should "test takeWhile and dropWhile" in {
+    val lists = Gen.listOf(Gen.choose(0, 100))
+    val prop = forAll(lists) { list =>
+      val lessThan50: Int => Boolean = _ < 50
+      list.takeWhile(lessThan50) ++ list.dropWhile(lessThan50) == list
+    }
+    Prop.run(prop)
+  }
+
+  "Exercise 8.19" should "generate meaningful functions" in {
+    /*
+    https://github.com/fpinscala/fpinscala/blob/master/answerkey/testing/19.answer.markdown
+     */
+  }
+
+  "Exercise 8.20" should "experiment with the prop testing lib" in {
+    /*
+    Experiment
+     */
+  }
+}
+
+object ** {
+  def unapply[A, B](p: (A, B)) = Some(p)
 }

71028fc26aacb21ce80440df3a171cc07d9ce3c6
Ilya Zinkovich <ilya.zinkovich@gmail.com>
2019-01-29 19:52:22 +0100

    Added a couple of solutions for Applicative exercises

--- src/test/scala/fpinscala/_12applicative/Applicative.scala
+++ src/test/scala/fpinscala/_12applicative/Applicative.scala
@@ -4,9 +4,11 @@ import fpinscala._11monad.Functor
 
 trait Applicative[F[_]] extends Functor[F] {
 
-  def unit[A](a: A): F[A]
+  def unit[A](a: => A): F[A]
 
   def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
+
+  def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] = map2(fa, fab)((a, fa) => fa(a))
 
   def map[A, B](fa: F[A])(f: A => B): F[B] = map2(fa, unit(()))((a, _) => f(a))
 
@@ -18,4 +20,36 @@ trait Applicative[F[_]] extends Functor[F] {
   def replicateM[A](n: Int, fa: F[A]): F[List[A]] = sequence(List.fill(n)(fa))
 
   def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] = map2(fa, fb)((_, _))
+
+  def mapViaApply[A, B](fa: F[A])(f: A => B): F[B] = apply(unit(f))(fa)
+
+  def map2ViaApply[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    apply(apply(unit(f.curried))(fa))(fb)
+
+  def map3[A, B, C, D](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => D): F[D] =
+    apply(apply(apply(unit(f.curried))(fa))(fb))(fc)
+
+  def map4[A, B, C, D, E](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A, B, C, D) => E): F[E] =
+    apply(apply(apply(apply(unit(f.curried))(fa))(fb))(fc))(fd)
+
+  def product[G[_]](G: Applicative[G]): Applicative[({type f[x] = (F[x], G[x])})#f] = {
+    val self = this
+    new Applicative[({type f[x] = (F[x], G[x])})#f] {
+      def unit[A](a: => A): (F[A], G[A]) = (self.unit(a), G.unit(a))
+
+      def map2[A, B, C](fa: (F[A], G[A]), fb: (F[B], G[B]))(f: (A, B) => C): (F[C], G[C]) =
+        (self.map2(fa._1, fb._1)(f), G.map2(fa._2, fb._2)(f))
+    }
+  }
+
+  def compose[G[_]](G: Applicative[G]): Applicative[({type f[x] = F[G[x]]})#f] = {
+    val self = this
+    new Applicative[({type f[x] = F[G[x]]})#f] {
+      def unit[A](a: => A): F[G[A]] = self.unit(G.unit(a))
+
+      def map2[A, B, C](fa: F[G[A]], fb: F[G[B]])(f: (A, B) => C): F[G[C]] =
+        self.map2(fa, fb)((ga, gb) => G.map2(ga, gb)(f))
+    }
+  }
+
 }
--- src/test/scala/fpinscala/_12applicative/ApplicativeExercises.scala
+++ src/test/scala/fpinscala/_12applicative/ApplicativeExercises.scala
@@ -1,10 +1,121 @@
 package fpinscala._12applicative
 
+import fpinscala._11monad.Monad
 import org.scalatest.{FlatSpec, Matchers}
 
 class ApplicativeExercises extends FlatSpec with Matchers {
 
-  "Exercise 12.1" should "sequence, replicateM and product" in {
+  private val optionApplicative = new Applicative[Option] {
+    def unit[A](a: => A): Option[A] = Some(a)
 
+    def map2[A, B, C](fa: Option[A], fb: Option[B])(f: (A, B) => C): Option[C] = (fa, fb) match {
+      case (Some(a), Some(b)) => Some(f(a, b))
+      case _ => None
+    }
+  }
+
+  private def listApplicative = new Applicative[List] {
+    def unit[A](a: => A): List[A] = List(a)
+
+    def map2[A, B, C](fa: List[A], fb: List[B])(f: (A, B) => C): List[C] =
+      fa.flatMap(a => fb.map(b => f.apply(a, b)))
+  }
+
+  "Exercise 12.1" should "sequence, replicateM and product" in {
+    optionApplicative.sequence(List(Some(1), Some(2))) should be(Some(List(1, 2)))
+    optionApplicative.sequence(List(Some(1), None)) should be(None)
+    optionApplicative.replicateM(2, Some(1)) should be(Some(List(1, 1)))
+    optionApplicative.replicateM(2, None) should be(None)
+    optionApplicative.product(Some(1), Some(2)) should be(Some((1, 2)))
+    optionApplicative.product(Some(1), None) should be(None)
+  }
+
+  "Exercise 12.2" should "apply via map, map via apply, map2 via apply" in {
+    optionApplicative.apply(Some[Int => Int](_ * 2))(Some(1)) should be(Some(2))
+    optionApplicative.mapViaApply(Some(1))(_ * 2) should be(Some(2))
+    optionApplicative.map2ViaApply(Some(1), Some(2))(_ * _) should be(Some(2))
+  }
+
+  "Exercise 12.3" should "map3 and map4" in {
+    optionApplicative.map3(Some(1), Some(2), Some(3))(_ * _ * _) should be(Some(6))
+    optionApplicative.map4(Some(1), Some(2), Some(3), Some(4))(_ * _ * _ * _) should be(Some(24))
+  }
+
+  "Exercise 12.4" should "stream sequence" in {
+    val streamApplicative = new Applicative[Stream] {
+      def unit[A](a: => A): Stream[A] =
+        Stream.continually(a)
+
+      def map2[A, B, C](a: Stream[A], b: Stream[B])(f: (A, B) => C): Stream[C] =
+        a zip b map f.tupled
+    }
+
+    /*
+      Sequence transposes the list!
+      That is, we start with a list of rows, each of which is possibly infinite in length.
+      We get back a single row, where each element is the column of values at that position.
+     */
+    streamApplicative.sequence(List(Stream(1, 2, 3), Stream(4, 5), Stream(6, 7, 8))).toList should be(List(List(1, 4, 6), List(2, 5, 7)))
+  }
+
+  "Exercise 12.5" should "either monad" in {
+    def eitherMonad[E] = new Monad[({type f[x] = Either[E, x]})#f] {
+      def unit[A](a: => A): Either[E, A] = Right(a)
+
+      def flatMap[A, B](fa: Either[E, A])(f: A => Either[E, B]): Either[E, B] = fa match {
+        case Right(value) => f(value)
+        case Left(value) => Left(value)
+      }
+    }
+
+    val eithers = List(Right(1), Left("error1"), Left("error2"))
+    eitherMonad[String].sequence(eithers) should be(Left("error1"))
+  }
+
+  "Exercise 12.6" should "validation applicative" in {
+    def validationApplicative[E] = new Applicative[({type f[x] = Validation[E, x]})#f] {
+      def unit[A](a: => A): Validation[E, A] = Success(a)
+
+      def map2[A, B, C](fa: Validation[E, A], fb: Validation[E, B])(f: (A, B) => C): Validation[E, C] = (fa, fb) match {
+        case (Success(a), Success(b)) => Success(f(a, b))
+        case (failure@Failure(_, _), Success(_)) => failure
+        case (Success(_), failure@Failure(_, _)) => failure
+        case (Failure(headA, tailA), Failure(headB, tailB)) => Failure(headA, tailA ++ Vector(headB) ++ tailB)
+      }
+    }
+
+    val validations = List(Success(1), Failure("error1", Vector()), Failure("error2", Vector()))
+    validationApplicative[String].sequence(validations) should be(Failure("error1", Vector("error2")))
+  }
+
+  "Exercise 12.7" should "prove monads are applicatives" in {
+    /*
+      map2(unit(()), fa)((_,a) => a) == fa // Left identity
+      flatMap(unit())(u => map(fa)(a => a)) == fa
+      flatMap(unit())(u => fa) == fa
+      compose(unit, u => fa)(()) == fa
+      compose(unit, f) == f
+      (u => fa)(()) == fa
+      fa == fa
+
+      map2(fa, unit(()))((a,_) => a) == fa // Right identity
+      flatMap(fa)(a => map(unit(()))(u => a)) == fa
+      flatMap(fa)(a => unit(a)) == fa  // via functor laws
+      compose(u => fa, unit)(()) == fa
+      (u => fa)(()) == fa
+      fa == fa
+
+      Associativity and naturality are TBD.
+     */
+  }
+
+  "Exercise 12.8" should "applicatives product" in {
+    val optionListApplicative = optionApplicative.product(listApplicative)
+    optionListApplicative.sequence(List((Some(1), List(2)), (None, List(3)))) should be((None, List(List(2, 3))))
+  }
+
+  "Exercise 12.9" should "applicatives compose" in {
+    val optionListApplicative = optionApplicative.compose(listApplicative)
+    optionListApplicative.sequence(List(Some(List(1, 2)), Some(List(3)))) should be(Some(List(List(1, 3), List(2, 3))))
   }
 }
